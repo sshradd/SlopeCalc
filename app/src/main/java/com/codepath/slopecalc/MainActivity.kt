a0bfde8f56e7eccc74b3de8694b2c124c7c1faac
shrad <shraddhasubashpublic@gmail.com>
2023-10-11 18:10:05 -0500

    error fixed 1

--- app/src/main/java/com/codepath/slopecalc/MainActivity.kt
+++ app/src/main/java/com/codepath/slopecalc/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.codepath.slopecalc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var X1input: EditText
+    private lateinit var Y1input: EditText
+    private lateinit var X2input: EditText
+    private lateinit var Y2input: EditText
+    private lateinit var slope: TextView
+    private lateinit var button: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        button = findViewById(R.id.button)
+        X1input = findViewById(R.id.X1input)
+        X2input = findViewById(R.id.X2input)
+        Y1input = findViewById(R.id.Y1input)
+        Y2input = findViewById(R.id.Y2input)
+        slope = findViewById(R.id.slope)
+        // call calculation in onclicklistener method
     }
 }
