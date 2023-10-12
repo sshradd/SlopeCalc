463e91defcfbd16952a2a505303d7e02f9c0b1b2
shrad <shraddhasubashpublic@gmail.com>
2023-10-12 08:28:10 -0500

    error fixed 1

--- app/src/main/java/com/codepath/slopecalc/MainActivity.kt
+++ app/src/main/java/com/codepath/slopecalc/MainActivity.kt
@@ -3,9 +3,12 @@ package com.codepath.slopecalc
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var X2input: EditText
     private lateinit var Y2input: EditText
     private lateinit var slope: TextView
+    private lateinit var slopeAMT: TextView
     private lateinit var button: Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +30,23 @@ class MainActivity : AppCompatActivity() {
         Y1input = findViewById(R.id.Y1input)
         Y2input = findViewById(R.id.Y2input)
         slope = findViewById(R.id.slope)
+        slopeAMT = findViewById(R.id.slopeAMT)
         // call calculation in onclicklistener method
+
+        button.setOnClickListener {
+            val x1 = X1input.text.toString().toDouble()
+            val x2 = X2input.text.toString().toDouble()
+            val y1 = Y1input.text.toString().toDouble()
+            val y2 = Y2input.text.toString().toDouble()
+            val result = (y2-y1)/(x2-x1)
+            slopeAMT.text = result.toString()
+            Toast.makeText(this, "Slope Calculated!", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
     }
+
+
 }
